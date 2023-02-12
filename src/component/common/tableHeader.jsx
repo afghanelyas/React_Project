bd08448c00a72f486e9e959673a9ad81580ca5a1
afghanelyas <afghan.elyas1@gmail.com>
2023-02-12 11:24:07 +0430

    solve the sorting extarcting tableheader

---
+++ src/component/common/tableHeader.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+class TableHeader extends Component {
+   
+    raiseSort = path => {
+        const sortColumn = {...this.props.sortColumn};
+        if (sortColumn.path === path) {
+            sortColumn.order = sortColumn.order === "asc" ? "desc" : "asc";
+        }else{
+            sortColumn.path = path;
+            sortColumn.order = 'asc';
+        }
+        this.props.onSort(sortColumn);
+    }
+
+    render() { 
+        return(
+            <thead className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">
+                <tr>
+                    {this.props.columns.map(column => (
+                        <th key={column.path || column.key} onClick={() => this.raiseSort(column.path)}>{column.label}</th>
+                    ))}
+                </tr>
+            </thead>
+        )
+    }
+}
+ 
+export default TableHeader;
